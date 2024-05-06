1ebdc6094551da4927cc9805dcfc209b19ee92a8
Kite Lin <jhbelieve0712@gmail.com>
2024-05-06 22:36:11 +0800

    chore(module): add roll call module

---
+++ Modules/RollCall/Database/Seeders/RollCallDatabaseSeeder_2024_05_06_142413.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\RollCall\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RollCallDatabaseSeeder_2024_05_06_142413 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
